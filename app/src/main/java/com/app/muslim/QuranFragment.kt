87d9b49ab8088022874bae99ef34fc4be29e8885
Gilang <initial.gr.101@gmail.com>
2020-12-10 19:22:59 +0700

    Design RecipesFragment Layout

--- app/src/main/java/com/app/muslim/QuranFragment.kt
+++ app/src/main/java/com/app/muslim/QuranFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_quran.view.*
 
 class QuranFragment : Fragment() {
 
@@ -13,6 +14,10 @@ class QuranFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quran, container, false)
+        val view = inflater.inflate(R.layout.fragment_quran, container, false)
+
+        view.recyclerview.showShimmer()
+
+        return view
     }
 }
